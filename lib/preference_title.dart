8c068d61e1d21ce2d08cfb83502ca3e57fd24589
redsolver <xxredsolverxx@gmail.com>
2019-05-06 19:26:12 +0200

    PreferenceTitle: Added Option to override left padding

--- lib/preference_title.dart
+++ lib/preference_title.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 class PreferenceTitle extends StatelessWidget {
   final String title;
+  final double leftPadding;
   final TextStyle style;
 
-  PreferenceTitle(this.title, {this.style});
+  PreferenceTitle(this.title, {this.leftPadding = 10.0, this.style});
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10.0, bottom: 0.0, top: 20.0),
+      padding: EdgeInsets.only(left: leftPadding, bottom: 0.0, top: 20.0),
       child: Text(
         title,
         style: style ??
