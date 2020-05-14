820bf13aa5eaebf5775d6a301a7ef732bbb0085b
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-05-14 08:53:59 -0400

    Remove deprecated functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,12 @@
+# Changelog
+
+## 6.0.0
+
+- Rewrite Pref service
+- Reorganize the library
+- Rename the library
+- Remove deprecated functions
+
 ## [5.2.0] - 06.04.2020
 
 - Fixed types in `DropdownPreference` (thx to @davbfr)
@@ -13,64 +22,64 @@
 
 ## [5.0.1] - 04.10.2019
 
-* You can now use `PrefService` without `init()` if you use caching.
+- You can now use `PrefService` without `init()` if you use caching.
 
 ## [5.0.0] - 03.10.2019
 
-* Added validation for `TextFieldPreference`
-* Added `obscureText`, `hintText`, `style`, `keyboardType`, `labelStyle` and `decoration` for `TextFieldPreference`
-* Added `disabled` option for every widget
-* Added `leading` and `subtitle` for `PreferenceText`
+- Added validation for `TextFieldPreference`
+- Added `obscureText`, `hintText`, `style`, `keyboardType`, `labelStyle` and `decoration` for `TextFieldPreference`
+- Added `disabled` option for every widget
+- Added `leading` and `subtitle` for `PreferenceText`
 
 ## [4.0.0] - 11.06.2019
 
-* Added setDefaultValues function to PrefService
-* CheckboxPreference, DropdownPreference, SwitchPreference, TextFieldPreference: Default value of the widget is now set and saved when first rendered
-* Added possibility to modify saved text in TextFieldPreference by returning custom string in the onChange Method
+- Added setDefaultValues function to PrefService
+- CheckboxPreference, DropdownPreference, SwitchPreference, TextFieldPreference: Default value of the widget is now set and saved when first rendered
+- Added possibility to modify saved text in TextFieldPreference by returning custom string in the onChange Method
 
 ## [3.0.0] - 06.05.2019
 
-* Added TextField preference
-* PreferenceTitle: Added Option to override left padding
-* PreferenceDialogLink: Added onPop callback
-* Fixed Prefix not applying when caching
+- Added TextField preference
+- PreferenceTitle: Added Option to override left padding
+- PreferenceDialogLink: Added onPop callback
+- Fixed Prefix not applying when caching
 
 ## [2.0.0] - 30.01.2019
 
-* Upgraded shared_preferences to 0.5.2
+- Upgraded shared_preferences to 0.5.2
 
 ## [1.3.0] - 26.01.2019
 
-* Fixed Issue with PrefService prefix not applying
+- Fixed Issue with PrefService prefix not applying
 
 ## [1.2.0] - 20.01.2019
 
-* Added PreferenceDialog and PreferenceDialogLink
-* Added cache for PrefService to cache values and only apply them when submitting a dialog or confirming a change
-* Added StringList Getter and Setter to PrefService
-* Fixed some issues
+- Added PreferenceDialog and PreferenceDialogLink
+- Added cache for PrefService to cache values and only apply them when submitting a dialog or confirming a change
+- Added StringList Getter and Setter to PrefService
+- Fixed some issues
 
 ## [1.1.0] - 17.01.2019
 
-* Fixed Issue with default RadioPreference being not first
+- Fixed Issue with default RadioPreference being not first
 
 ## [1.0.8] - 05.01.2019
 
-* Fixed Issue with PreferenceHider
+- Fixed Issue with PreferenceHider
 
 ## [1.0.7] - 05.01.2019
 
-* Fixed Issue with RadioPreference widgets on subpage
+- Fixed Issue with RadioPreference widgets on subpage
 
 ## [1.0.5] - 05.01.2019
 
-* DropdownPreference Displayed Values can now be different from saved values
-* PreferencePageLink Page Title can now be different from Link-Label
+- DropdownPreference Displayed Values can now be different from saved values
+- PreferencePageLink Page Title can now be different from Link-Label
 
 ## [1.0.3] - 01.01.2019
 
-* You can now init PrefService again with a different prefix
+- You can now init PrefService again with a different prefix
 
 ## [1.0.0] - 31.12.2018
 
-* Initial release.
+- Initial release.
--- lib/src/service/pref_service.dart
+++ lib/src/service/pref_service.dart
@@ -3,13 +3,11 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
 
 import 'base.dart';
-import 'shared_preferences.dart';
 
 class PrefService extends InheritedWidget {
   const PrefService({
@@ -23,12 +21,6 @@ class PrefService extends InheritedWidget {
 
   static BasePrefService _instance;
 
-  @deprecated
-  final Map cache = const {};
-
-  @deprecated
-  final String prefix = '';
-
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => false;
 
@@ -36,162 +28,9 @@ class PrefService extends InheritedWidget {
       context.dependOnInheritedWidgetOfExactType<PrefService>().service ??
       _instance;
 
-  @Deprecated('Use PrefService as a widget instead')
-  static Future<bool> init({
-    @Deprecated('Use service instead') String prefix = '',
-    BasePrefService service,
-  }) async {
-    if (service != null) {
-      _instance = service;
-      return true;
-    }
-
-    if (_instance != null) {
-      return false;
-    }
-
-    _instance = await SharedPrefService.init(prefix: prefix);
-    return true;
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void setDefaultValues(Map<String, dynamic> values) {
-    checkInit();
-    _instance.setDefaultValues(values);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static bool getBool(String key) {
-    checkInit();
-    return _instance.getBool(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static FutureOr<bool> setBool(String key, bool val) {
-    checkInit();
-    return _instance.setBool(key, val);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static String getString(String key) {
-    checkInit();
-    return _instance.getString(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static FutureOr<bool> setString(String key, String val) {
-    checkInit();
-    return _instance.setString(key, val);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static int getInt(String key) {
-    checkInit();
-    return _instance.getInt(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static FutureOr<bool> setInt(String key, int val) {
-    checkInit();
-    return _instance.setInt(key, val);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static double getDouble(String key) {
-    checkInit();
-    return _instance.getDouble(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static FutureOr<bool> setDouble(String key, double val) {
-    checkInit();
-    return _instance.setDouble(key, val);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static List<String> getStringList(String key) {
-    checkInit();
-    return _instance.getStringList(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static FutureOr<bool> setStringList(String key, List<String> val) {
-    checkInit();
-    return _instance.setStringList(key, val);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static dynamic get(String key) {
-    checkInit();
-    return _instance.get(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static Set<String> getKeys() {
-    checkInit();
-    return _instance.getKeys();
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void notify(String key) {
-    checkInit();
-    _instance.notify(key);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void onNotify(String key, VoidCallback f) {
-    checkInit();
-    _instance.onNotify(key, f);
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void onNotifyRemove(String key, VoidCallback f) {
-    checkInit();
-    _instance.onNotifyRemove(key, f);
-  }
-
   static void showError(BuildContext context, String message) {
     Scaffold.of(context).showSnackBar(SnackBar(
       content: Text(message),
     ));
   }
-
-  @Deprecated('Use PrefService.of()')
-  static void checkInit() {
-    if (_instance == null) {
-      throw Exception('''\n
-  PrefService not initialized.
-  Call await PrefService.init() before any other PrefService call.
-
-  main() async {
-    await PrefService.init();
-    runApp(MyApp());
-  }
-      ''');
-    }
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void clear() {
-    checkInit();
-    _instance.clear();
-  }
-
-  @Deprecated('Use PrefService.of()')
-  static void apply(BasePrefService other) {
-    checkInit();
-    _instance.apply(other);
-  }
-
-  @Deprecated('You can safely remove the call to this function')
-  static void rebuildCache() {}
-
-  @Deprecated('Use JustCachePrefService')
-  static void enableCaching() {}
-
-  @Deprecated('Use SharedPrefService')
-  static void disableCaching() {}
-
-  @Deprecated('use apply(other)')
-  static Future<void> applyCache() async {}
 }
